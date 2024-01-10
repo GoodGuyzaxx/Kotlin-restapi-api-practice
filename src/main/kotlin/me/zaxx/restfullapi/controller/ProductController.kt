503f83c08fae2502c2c1d3e46c3378cd8521073d
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2024-01-11 03:20:39 +0900

    fix error POST and add GET method

--- src/main/kotlin/me/zaxx/restfullapi/controller/ProductController.kt
+++ src/main/kotlin/me/zaxx/restfullapi/controller/ProductController.kt
@@ -4,24 +4,40 @@ import me.zaxx.restfullapi.model.CreateProductRequest
 import me.zaxx.restfullapi.model.ProductResponse
 import me.zaxx.restfullapi.model.WebResponse
 import me.zaxx.restfullapi.service.ProductService
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class ProductController(val productService: ProductService) {
 
     @PostMapping(
-        value = ["api/products"],
-        produces = ["application/json"],
-        consumes = ["application/json"]
+        value = ["/api/products"],
+        produces = ["application/json","application/x-www-form-urlencoded"],
+        consumes = ["application/json","application/x-www-form-urlencoded","multipart/form-data"],
     )
-    fun createProduct(@RequestBody body: CreateProductRequest): WebResponse<ProductResponse>{
+    fun createProduct(body: CreateProductRequest): WebResponse<ProductResponse>{
         val productResponse = productService.create(body)
         return WebResponse(
             code = 200,
             status = "OK",
             data = productResponse
+        )
+    }
+
+    @GetMapping(
+            value = ["/api/products/{id}"]
+    )
+    fun getProduct(@PathVariable("id") id: String): WebResponse<ProductResponse>{
+        val productResponse = productService.get(id)
+        return WebResponse(
+                code = 200,
+                status = "OK",
+                data = productResponse
         )
     }
 }
