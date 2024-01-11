5a4045744e98a281353ccdd4222d75684fc6f348
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2024-01-12 04:20:41 +0900

    Update UpdateProductRequest.kt

--- src/main/kotlin/me/zaxx/restfullapi/model/UpdateProductRequest.kt
+++ src/main/kotlin/me/zaxx/restfullapi/model/UpdateProductRequest.kt
@@ -3,6 +3,7 @@ package me.zaxx.restfullapi.model
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
+import java.util.Date
 
 data class UpdateProductRequest(
 
@@ -15,5 +16,7 @@ data class UpdateProductRequest(
 
         @field:NotNull
         @field:Min(0)
-        val quantity: Int
+        val quantity: Int,
+
+        val updatedAt: Date
 )
